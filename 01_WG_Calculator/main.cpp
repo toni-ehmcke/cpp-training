ce611792102e3335d22c2b862a6eeffc8a4ee8ce
toni-ehmcke <t.ehmcke@web.de>
2015-04-10 15:11:24 +0200

    Added first user inputs

--- 01_WG_Calculator/main.cpp
+++ 01_WG_Calculator/main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+/*
+ *  Name: DicoFair (Distribute costs fairly)
+ *  Author: Toni Ehmcke (April 2015)
+ *  There are many situations where several people spent money on particular products
+ *  and now one has to distribute the costs fairly. This programm calculates a balance
+ *  for each of the participants that represents the amount of money one receives/has to pay.
+ */
 
-using namespace std;
+#include <iostream>
+#include <string>
+#include <sstream>
+
+struct person
+{
+    std::string name;
+    double moneyPaid;
+    double moneyBalance;
+};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout << "___________________Welcome to DicoFair!___________________" << std::endl << std::endl;
+    std::string inputString;
+    unsigned int numPerson = 0;
+    while (numPerson <= 0)
+    {
+        std::cout << "Enter the number of persons that share the costs: ";
+        std::getline(std::cin, inputString);
+        std::stringstream(inputString) >> numPerson;
+        if (numPerson <= 0)
+            std::cout << "Please enter a valid number of people!" << std::endl;
+    }
+
     return 0;
 }
